a25a9fd1da55babcae2119babc46ce626b2dba39
cbravo20 <cbravo-perez@csumb.edu>
2025-04-22 23:58:35 -0700

    Fixed delete/review endpoints
    Fixed delete endpoint for lists

--- proj3/src/main/java/com/example/proj3/repository/UserRepository.java
+++ proj3/src/main/java/com/example/proj3/repository/UserRepository.java
@@ -5,14 +5,14 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
+import java.util.Optional;
 import com.example.proj3.model.User;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     List<User> findAll();
 
-    User findByUsername(String username);
+    Optional<User> findByUsername(String username);
 
     User findByEmail(String email);
 
